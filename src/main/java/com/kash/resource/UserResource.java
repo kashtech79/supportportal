3b5d146841477c923ba7077bad845248a68fec82
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-10-30 12:46:29 +0200

    39. Custom exception handling

--- src/main/java/com/kash/resource/UserResource.java
+++ src/main/java/com/kash/resource/UserResource.java
@@ -1,12 +1,13 @@
 package com.kash.resource;
 
+import com.kash.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/user")
     public String showUser(){
