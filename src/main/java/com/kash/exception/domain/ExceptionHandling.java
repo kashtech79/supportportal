8e3afd7e6827ed7aa596d3041d0f7866bf198278
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-10-30 16:27:12 +0200

    41. Custom exception handling part 3

--- src/main/java/com/kash/exception/domain/ExceptionHandling.java
+++ src/main/java/com/kash/exception/domain/ExceptionHandling.java
@@ -4,15 +4,21 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.kash.domain.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.NoResultException;
+import java.io.IOException;
+import java.util.Objects;
 
 import static org.springframework.http.HttpStatus.*;
 
---
+++ src/main/java/com/kash/exception/domain/NotAnImageFileException.java
@@ -0,0 +1,6 @@
+package com.kash.exception.domain;
+
+public class NotAnImageFileException extends Exception {
+    public NotAnImageFileException(String message) {
+        super(message);
+    }
