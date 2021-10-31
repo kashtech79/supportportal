2a0321fe5aca44557410bb316bb49ab688d1e50a
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-10-31 15:09:16 +0100

    45. User Roles

---
+++ src/main/java/com/kash/enumeration/Role.java
@@ -0,0 +1,20 @@
+package com.kash.enumeration;
+
+import static com.kash.constant.Authority.*;
+
+public enum Role {
+    ROLE_USER(USER_AUTHORITIES),
+    ROLE_HR(HR_AUTHORITIES),
+    ROLE_MANAGER(MANAGER_AUTHORITIES),
+    ROLE_ADMIN(ADMIN_AUTHORITIES),
+    ROLE_SUPER_ADMIN(SUPER_USER_AUTHORITIES);
+
+    private String[] authorities;
+    Role(String... authorities ){
+        this.authorities = authorities;
+    }
+
+    public String[] getAuthorities(){
+        return authorities;
+    }
+}
