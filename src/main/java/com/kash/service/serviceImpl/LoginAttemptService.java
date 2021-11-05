29532999642ac1ba0e6a16ac7a365b12fc8f201e
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-05 11:46:53 +0100

    59. Brute force attach part 2

--- src/main/java/com/kash/service/serviceImpl/LoginAttemptService.java
+++ src/main/java/com/kash/service/serviceImpl/LoginAttemptService.java
@@ -5,6 +5,8 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutionException;
+
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 @Service
@@ -22,5 +24,22 @@ public class LoginAttemptService {
                     }
                 });
     }
-}
+
+    public void evictUserFromLoginAttemptCache(String username) {
+        loginAttemptCache.invalidate(username);
+    }
+
+    public void addUserToLoginAttemptCache(String username) {
+        int attempts = 0;
+        try {
+            attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        loginAttemptCache.put(username, attempts);
+    }
+
+    public boolean hasExceededMaxAttempts(String username) {
+            return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
+    }
 }
