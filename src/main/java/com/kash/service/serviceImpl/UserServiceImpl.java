c9895bd243e39d35f408aae9bfdc25197b443b4f
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-09 11:58:05 +0100

    71. User Service Implementation part 1

--- src/main/java/com/kash/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/kash/service/serviceImpl/UserServiceImpl.java
@@ -2,10 +2,13 @@ package com.kash.service.serviceImpl;
 
 import com.kash.domain.User;
 import com.kash.domain.UserPrincipal;
+import com.kash.enumeration.Role;
 import com.kash.exception.domain.EmailExistException;
+import com.kash.exception.domain.UserNotFoundException;
 import com.kash.exception.domain.UsernameExistException;
 import com.kash.repository.UserRepository;
 import com.kash.service.UserService;
+import javassist.bytecode.MethodParametersAttribute;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.mail.MessagingException;
@@ -26,6 +30,7 @@ import java.util.List;
 
 import static com.kash.constant.UserImplConstant.*;
 import static com.kash.enumeration.Role.ROLE_USER;
+import static jdk.internal.joptsimple.internal.Strings.EMPTY;
 
 @Service
 @Transactional
@@ -106,9 +111,58 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         return user;
     }
 
+    @Override
+    public User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException {
+        validNewUsernameAndEmail(EMPTY, username, email);
+        User user = new User();
+        String password = generatePassword();
+        user.setUserId(generateUserId());
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setJoinDate(new Date());
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(encodePassword(password));
+        user.setActive(isActive);
+        user.setNotLocked(isNonLocked);
+        user.setRole(getRoleEnumName(role).name());
+        user.setAuthorities(getRoleEnumName(role).getAuthorities());
+        user.setProfileImageUrl(getTemporaryProfileImageUrl(username));
+        userRepository.save(user);
+        saveProfileImage(user, profileImage);
+        LOGGER.info("New user password: " + password);
+        return user;
+
+    }
+
+    private void saveProfileImage(User user, MultipartFile profileImage) {
+    }
+
+    private Role getRoleEnumName(String role) {
+    }
+
+    @Override
+    public User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void resetPassword(String email) {
+
+    }
+
+    @Override
+    public User updateProfileImage(String username, MultipartFile profileImage) {
+        return null;
+    }
+
     //10
-    private String getTemporaryProfileImageUrl() {
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path(DEFAULT_USER_IMAGE_PATH).toUriString();
+    private String getTemporaryProfileImageUrl(String username) {
     }
 
     //9
