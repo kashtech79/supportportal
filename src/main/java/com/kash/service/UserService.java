3a2e09fddb8537efa18af7dc3f3e131efdaead7b
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-09 11:22:55 +0100

    70. User Service

--- src/main/java/com/kash/service/UserService.java
+++ src/main/java/com/kash/service/UserService.java
@@ -5,6 +5,7 @@ import com.kash.exception.domain.EmailExistException;
 import com.kash.exception.domain.EmailNotFoundException;
 import com.kash.exception.domain.UserNotFoundException;
 import com.kash.exception.domain.UsernameExistException;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -18,5 +19,14 @@ public interface UserService {
 
     User findUserByEmail(String email);
 
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(String username);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 
 }
