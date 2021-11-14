455673043de4d12709334f0d4ca0bfe9311d96f1
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-14 11:09:40 +0100

    99. creating class

--- app/service/authentication.service.ts
+++ app/service/authentication.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
