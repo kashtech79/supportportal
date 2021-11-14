8694b263a0fd65972be8f802fdca49db4528165b
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-14 18:33:29 +0100

    109. Add user service call

--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -15,4 +15,8 @@ export class UserService {
   public getUser(): Observable<User[] | HttpErrorResponse>{
     return this.http.get<User[]>(`${this.host}/user/list`)
   }
+
+  public addUser(formData: FormData): Observable<any>{
+    return this.http.get<User[]>(`${this.host}/user/add`, formData);
+  }
 }
