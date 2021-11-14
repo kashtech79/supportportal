06e652c96f7c1cf4bcded6ccc4ca533499fa57ce
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-14 16:44:16 +0100

    108. User list service call

--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  public getUser(): Observable<User[] | HttpErrorResponse>{
+    return this.http.get<User[]>(`${this.host}/user/list`)
+  }
 }
