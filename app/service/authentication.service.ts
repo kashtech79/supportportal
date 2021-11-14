c786ee8c7a5d813cd78f426753c4655ef2d8189c
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-14 01:39:01 +0100

    95-98 login service call

---
+++ app/service/authentication.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private host = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+   public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse>{
+     return this.http.post<HttpResponse<any> | HttpErrorResponse>(`$(this.host}/user/login`, user, {observe: 'response'});
+   }
+}
