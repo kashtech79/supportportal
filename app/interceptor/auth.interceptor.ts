8489af7caab294fabdf1dea72697a94d34bbe4c7
kashifnizami@gmail.com <kashifnizami@gmail.com>
2021-11-15 11:15:08 +0100

    121. Creating Interceptor - Part 2

--- app/interceptor/auth.interceptor.ts
+++ app/interceptor/auth.interceptor.ts
@@ -17,5 +17,18 @@ export class AuthInterceptor implements HttpInterceptor {
     if(httprequest.url.includes(`${this.authenticationService.host}/user/login`)){
       return httpHandler.handle(httprequest)
     }
+
+      if(httprequest.url.includes(`${this.authenticationService.host}/user/register`)){
+      return httpHandler.handle(httprequest)
+    }
+
+      if(httprequest.url.includes(`${this.authenticationService.host}/user/resetpassword`)){
+      return httpHandler.handle(httprequest)
+    }
+
+    this.authenticationService.loadToken();
+    const token = this.authenticationService.getToken();
+    const request = httprequest.clone({setHeaders: {Authorization: `Bearer ${token}`}});
+    return httpHandler.handle(request);
   }
 }
